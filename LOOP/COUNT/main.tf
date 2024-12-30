c100dacbb4fb47e41e3b1ef6d2228ff6beabe37d
Unnati_v_kohale <kohaleunnati@gmail.com>
2024-12-30 15:36:10 +0530

    Update main.tf

--- LOOP/COUNT/main.tf
+++ LOOP/COUNT/main.tf
@@ -18,3 +18,26 @@ resource "aws_instance" "myserver" {
 
 }*/
 
+---------------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "myserver" {
+  ami = "ami-003f5a76758516d1e"
+  instance_type = "t2.micro"
+  key_name = "sydneykay"
+  count = 2
+
+  tags = {
+    Name = "instance-${count.index}"
+  }
+}
+
+
+
+output "instance_ip" {
+  value = aws_instance.myserver[*].public_ip 
+}
+
